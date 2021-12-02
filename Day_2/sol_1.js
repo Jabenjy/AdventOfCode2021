5ee9ce666cbe54c7417e6eb0bd222cee513770bc
BenAshing <jabenjy@gmail.com>
2021-12-02 12:14:59 +0000

    Small trim

--- Day_2/sol_1.js
+++ Day_2/sol_1.js
@@ -1,3 +1,3 @@
 i=require('fs').readFileSync('i','utf8').split(/\n/).map(a=>a.split(" "))
-for(n=u=f=d=0;n<i.length;x=i[n],l=x[0][0],y=x[1]- -0,l=='u'?u+=y:l=='d'?d+=y:f+=y,n++,console.log(l));
+for(n=u=f=d=0;n<i.length;x=i[n++],l=x[0][0],y=x[1]- -0,l=='u'?u+=y:l=='d'?d+=y:f+=y);
 console.log(f*(d-u))
--- Day_2/sol_2.js
+++ Day_2/sol_2.js
@@ -1,3 +1,3 @@
 i=require('fs').readFileSync('i','utf8').split(/\n/).map(a=>a.split(" "))
-for(a=n=f=d=0;n<i.length;x=i[n],l=x[0][0],y=x[1]- -0,l=='u'?a-=y:l=='d'?a+=y:(f+=y,d+=a*y),n++,console.log(l));
+for(a=n=f=d=0;n<i.length;x=i[n++],l=x[0][0],y=x[1]- -0,l=='u'?a-=y:l=='d'?a+=y:(f+=y,d+=a*y));
 console.log(f*d)
